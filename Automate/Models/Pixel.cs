0e19407417dd83195178f5a3f48a2b965f2982c4
UserIsntAvailable <PinedaP.Angel@gmail.com>
2020-12-13 10:02:50 -0500

    Pixel X and Y properties added

--- Automate/Models/Pixel.cs
+++ Automate/Models/Pixel.cs
@@ -5,5 +5,19 @@ namespace Automate.Models {
     /// <summary>
     /// A class declaring a pixel on a bitmap
     /// </summary>
-    public record Pixel((int X, int Y) Position, Color Color);
+    public record Pixel((int X, int Y) Position, Color Color) {
+
+        #region Public Properties
+
+        /// <summary>
+        /// The X position on the bitmap
+        /// </summary>
+        public int X => Position.X;
+
+        /// <summary>
+        /// The Y position on the bitmap
+        /// </summary>
+        public int Y => Position.Y;
+        #endregion
+    }
 }
